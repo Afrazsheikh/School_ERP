df453e3f8874f8fb4b8e230e6cd4c216a258befb
Gayatri Patel <gayatrimscit@gmail.com>
2023-05-27 13:08:19 +0530

    Fee structure changed as per give screen shot

--- src/app/pages/student-details/stu-info-fees/stu-info-fees.component.ts
+++ src/app/pages/student-details/stu-info-fees/stu-info-fees.component.ts
@@ -8,6 +8,7 @@ import { Router } from '@angular/router';
   templateUrl: './stu-info-fees.component.html',
   styleUrls: ['./stu-info-fees.component.scss']
 })
+
 export class StuInfoFeesComponent {
   @Input() studentData: any ;
   studentFee:any;
@@ -15,6 +16,15 @@ export class StuInfoFeesComponent {
   classes: any[] =[];
   concessionList:any[] = [];
   feeModeList:any[] =[];
+  row = {"isExpand": false}
+  optionsForQuaterly: any[] = [
+    {value:'Q1',months:['Jan','Feb','Mar']},
+    {value:'Q2',months:['Apr','May','Jun']},
+    {value:'Q3',months:['Jul','Aug','Sep']},
+    {value:'Q4',months:['Oct','Nov','Dec']}
+  ];
+  availableMonths = [{ months: 'January', value: 1 },{ months: 'February', value: 2 }, { months: 'March', value: 3 }, { months: 'April', value: 4 }, { months: 'May', value: 5 }, { months: 'June', value: 6 }, { months: 'July', value: 7 }, { months: 'August', value: 8 }, { months: 'September', value: 9 }, { months: 'October', value: 10 }, { months: 'November', value: 11}, { months: 'December', value: 12}]
+
   constructor(public fb: FormBuilder, private studentService:StudentService,private api: ApiService,private router: Router) {
     this.concessionList = this.studentService.concessionList;
     this.feeModeList = this.studentService.feeMode;
@@ -24,6 +34,7 @@ export class StuInfoFeesComponent {
   this.getAllClass();
   this.createForm();
  }
+
  getAllClass() {
   this.api.getAllClass().subscribe(resp => {
     this.classes = resp.classes
@@ -32,9 +43,9 @@ export class StuInfoFeesComponent {
  createForm(){
   this.studentForm = this.fb.group({ 
       id:[this.studentData._id],
-      feeType: ['', Validators.required],
-      concession: ['', Validators.required],
-      feeMode: ['', Validators.required]
+      feeType: ['', ],
+      concession: ['', ],
+      feeMode: ['', ]
     });
  }
  updateInfo(formData){
@@ -44,4 +55,5 @@ export class StuInfoFeesComponent {
  payInvoice(){
   this.router.navigate(["/student-acconting/pay-fees/"+this.studentData._id]);
  }
+ 
 }
