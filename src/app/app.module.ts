9675563af8fc11a4a127e8ff0dd503f2a0d04df9
Ankit Singh <aniketmodker24@gmail.com>
2023-06-02 22:17:31 +0530

    UI: Dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatButtonModule} from '@angular/material/button';
-import { SharedModule } from "./shared/shared.module";
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HumanRessourceComponent } from './pages/human-ressource/human-ressource.component';
 import { ReportsComponent } from './pages/reports/reports.component';
@@ -19,36 +19,38 @@ import { MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { SelectDropDownModule } from 'ngx-select-dropdown';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { NgxSpinnerModule } from "ngx-spinner";
-import {NgxPrintModule} from 'ngx-print';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { NgxPrintModule } from 'ngx-print';
+import { NgApexchartsModule } from 'ng-apexcharts';
 @NgModule({
-    declarations: [
-      AppComponent,
-      HumanRessourceComponent,
-      ReportsComponent,
-      DashboardComponent,
-    ],
-    imports: [
-      BrowserModule,
-      BrowserAnimationsModule,
-      NgxSpinnerModule,
-      AppRoutingModule,
-      HttpClientModule,
-      AppRoutingModule,
-      MatToolbarModule,
-      MatSidenavModule,
-      MatButtonModule,
-      MatIconModule,
-      SharedModule,
-      MatDatepickerModule,
-      MatNativeDateModule,
-      SelectDropDownModule,
-      NgxPrintModule,
-      ToastrModule.forRoot({positionClass: 'toast-center-center'}),
-      ModalModule.forRoot()
-    ],
-    providers: [BsModalService],
-    bootstrap: [AppComponent],
-    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  declarations: [
+    AppComponent,
+    HumanRessourceComponent,
+    ReportsComponent,
+    DashboardComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    NgxSpinnerModule,
+    AppRoutingModule,
+    HttpClientModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    SharedModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    SelectDropDownModule,
+    NgxPrintModule,
+    NgApexchartsModule,
+    ToastrModule.forRoot({ positionClass: 'toast-center-center' }),
+    ModalModule.forRoot(),
+  ],
+  providers: [BsModalService],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,147 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import {
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexDataLabels,
+  ApexPlotOptions,
+  ApexYAxis,
+  ApexXAxis,
+  ApexFill,
+  ApexTitleSubtitle,
+  ChartComponent,
+} from 'ng-apexcharts';
 
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  dataLabels: ApexDataLabels;
+  plotOptions: ApexPlotOptions;
+  yaxis: ApexYAxis;
+  xaxis: ApexXAxis;
+  fill: ApexFill;
+  title: ApexTitleSubtitle;
+};
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
+  @ViewChild('chart') chart: ChartComponent;
+  public chartOptions: Partial<ChartOptions>;
 
+  constructor() {
+    this.chartOptions = {
+      series: [
+        {
+          name: 'Fees Collected',
+          data: [230, 310, 400, 110, 400, 360, 320, 230, 140, 80, 50, 20],
+        },
+      ],
+      chart: {
+        height: 350,
+        type: 'bar',
+        toolbar: {
+          show: false,
+        },
+      },
+      plotOptions: {
+        bar: {
+          dataLabels: {
+            position: 'top', // top, center, bottom
+          },
+        },
+      },
+      dataLabels: {
+        enabled: true,
+        formatter: function (val) {
+          return val + '%';
+        },
+        offsetY: -20,
+        style: {
+          fontSize: '12px',
+          colors: ['#304758'],
+        },
+      },
+
+      xaxis: {
+        categories: [
+          'Jan',
+          'Feb',
+          'Mar',
+          'Apr',
+          'May',
+          'Jun',
+          'Jul',
+          'Aug',
+          'Sep',
+          'Oct',
+          'Nov',
+          'Dec',
+        ],
+        position: 'top',
+        labels: {
+          offsetY: -18,
+        },
+        axisBorder: {
+          show: false,
+        },
+        axisTicks: {
+          show: false,
+        },
+        crosshairs: {
+          fill: {
+            type: 'gradient',
+            gradient: {
+              colorFrom: '#D8E3F0',
+              colorTo: '#BED1E6',
+              stops: [0, 100],
+              opacityFrom: 0.4,
+              opacityTo: 0.5,
+            },
+          },
+        },
+        tooltip: {
+          enabled: true,
+          offsetY: -35,
+        },
+      },
+      fill: {
+        type: 'gradient',
+        gradient: {
+          shade: 'light',
+          type: 'horizontal',
+          shadeIntensity: 0.25,
+          gradientToColors: undefined,
+          inverseColors: true,
+          opacityFrom: 1,
+          opacityTo: 1,
+          stops: [50, 0, 100, 100],
+        },
+      },
+      yaxis: {
+        axisBorder: {
+          show: false,
+        },
+        axisTicks: {
+          show: false,
+        },
+        labels: {
+          show: false,
+          // formatter: function (val) {
+          //   return val + '%';
+          // },
+        },
+      },
+      title: {
+        text: 'Fees Collected, 2023-2024',
+        // floating: 0,
+        offsetY: 330,
+        align: 'center',
+        style: {
+          color: '#444',
+        },
+      },
+    };
+  }
 }
