e9d499d69d3f3e80bb4c8e518670e16fadeddb52
Afrazsheikh <sheikhafraz04@gmail.com>
2023-12-05 11:42:00 +0530

    Revert "Added Back button as well as sorting and searching code"
    This reverts commit 18414088abf0d5f6b78a48303823d6f56f370374.

--- src/app/pages/transport/vehicle-master/vehicle-master.component.ts
+++ src/app/pages/transport/vehicle-master/vehicle-master.component.ts
@@ -26,6 +26,7 @@ export class VehicleMasterComponent implements OnInit {
       driverName: new FormControl(null, [Validators.required]),
       driverPhoneNo: new FormControl(null, [Validators.required]),
       driverLicense: new FormControl(null, [Validators.required])
+      driverLicense: new FormControl(null, [Validators.required]),
     });
   }
 
